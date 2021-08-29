e55d0fa8d42c12b0cea78a2592ecd70f045261a2
Dani-Iliev <75176422+Dani-Iliev@users.noreply.github.com>
2021-08-29 23:43:40 +0300

    Add If Else statement

---
+++ IfElse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int c;
+
+            if (a < b)
+            {
+                c = a + b;
+            }
+            else if (a > b)
+            {
+                c = a - b;
+            }
+            else
+            {
+                c = a * b;
+            }
+            Console.WriteLine(c);
+        }
+    }
+}
